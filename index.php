3943ac2a8bbe3cb1e7ae0aeb7c343d50043843be
Flor De Yared <flordeyared@gmail.com>
2016-03-04 01:24:06 -0500

    primer mundo

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="contenedor">
+	<nav>
+	<h1>FAMILIA CHAPARRO ESTRELLA</h1>
+		<ul class= "menu-lista">
+			<li class="menu-item">
+			<a href="" class="menu-link">INICIO</a>
+			</li>
+			<li class="menu-item">
+			<a href=""class="menu-link">GALERIA</a>
+			</li>
+			<li class="menu-item">
+			<a href="" class="menu-link">BLOCK</a>
+			</li>
+			<li class="menu-item">
+			<a href="" class="menu-link">ANUNCIOS</a>
+			</li>
+            <li class="menu-item">
+			<a href="" class="menu-link">CONTACTENOS</a>
+			</li>
+		</ul>
+	</nav>
+	<div class="slider">
+	<img src="12.jpg" height="350" width="960">
+	</P>
+	</nav>
+	<div class="slider">
+	<img src="4.jpg" height="180" nidth"180">
+
+	<img src="yare.jpg" height="180" nidth"180">
+	
+    <img src="nando.jpg" height="180" nidth"180">
+    
+	<img src="sayuri.jpg" height="180" nidth"180">
+
+</div>
+</body>
+</html>
